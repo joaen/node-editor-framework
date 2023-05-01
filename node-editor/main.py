7514a540825235cfb7136aeb8c01731ed2149224
Joaen <joar.engberg@gmail.com>
2023-05-01 19:06:04 +0200

    new files

--- node-editor/main.py
+++ node-editor/main.py
@@ -1,27 +1,9 @@
 import sys
-
-from PySide2.QtCore import QSize, Qt
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
+from main_window import MainWindow
 
-
-# Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("My App")
-
-        button = QPushButton("Press Me!")
-
-        self.setFixedSize(QSize(400, 300))
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
