eb3b4e581349d70988ae55ec925dc3508ea0d4c0
Joaen <joar.engberg@gmail.com>
2023-05-06 00:23:56 +0200

    added color change when clicking port

--- node-editor/graphics_circle.py
+++ node-editor/graphics_circle.py
@@ -3,15 +3,28 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
 class GraphicsCircle(QGraphicsItem):
-    
+
     def __init__(self, x=200, y=200, radius=20):
         super().__init__()
         self.x = x
         self.y = y
         self.radius = radius
+        self.color = QColor(255, 165, 0)
 
     def mousePressEvent(self, event):
-        print("Circle clicked!")
+        print("PRESSED")
+        if event.button() == Qt.LeftButton:
+            self.color = QColor(255, 0, 0)
+            self.update()
+        else:
+            return super().mousePressEvent(event)
+        
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if event.button() == Qt.LeftButton:
+            self.color = QColor(255, 165, 0)
+            self.update()
+        else:
+            return super().mouseReleaseEvent(event)
 
     def boundingRect(self):
         return QRectF(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
@@ -20,5 +33,5 @@ class GraphicsCircle(QGraphicsItem):
         pen = QPen(QColor(255, 0, 0))
         pen.setWidth(2)
         painter.setPen(pen)
-        painter.setBrush(QColor(0, 255, 0))
+        painter.setBrush(self.color)
         painter.drawEllipse(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
