0c6900ac2d447b914756856c7e7fe74f1dc0de25
Joaen <joar.engberg@gmail.com>
2023-07-14 00:52:14 +0200

    FINALLY FIXED THE LINES SO THEY ARE CLICKABLE

--- node-editor/graphics/graphics_line.py
+++ node-editor/graphics/graphics_line.py
@@ -4,7 +4,7 @@ from PySide2.QtGui import *
 from graphics.graphics_port import GraphicsPort
 
 
-class GraphicsLine(QGraphicsItem):
+class GraphicsLine(QGraphicsPathItem):
 
     def __init__(self, port_one : GraphicsPort, port_two : GraphicsPort):
         super().__init__()
@@ -14,24 +14,31 @@ class GraphicsLine(QGraphicsItem):
         self.start_point_y = self.port_one.port_pos().y()
         self.end_point_x = self.port_two.port_pos().x()
         self.end_point_y = self.port_two.port_pos().y()
-        self.path = QPainterPath()
+        self.test_path = QPainterPath()
         self.color = QColor(255, 255, 255)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
 
-    # def mousePressEvent(self, event):
-    #     print("PRESSED LINE")
-    #     super().mousePressEvent(event)
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            print("PRESSED LINE")
+
+        super().mousePressEvent(event)
 
     def boundingRect(self):
-        return self.path.boundingRect()
+        return self.test_path.boundingRect()
 
-    def paint(self, painter: QPainter, option, widget):
-        self.path.clear()
-        pen = QPen(self.color)
-        pen.setWidth(2)
+    def paint(self, painter: QPainter, option, widget=None):
+        self.test_path.clear()
+
+        if self.isSelected():
+            pen = QPen(QColor(0, 255, 0))
+        else:
+            pen = QPen(self.color)
+        pen.setWidth(4)
         painter.setPen(pen)
 
         start_point = QPointF(self.start_point_x, self.start_point_y)
-        self.path.moveTo(start_point)
+        self.test_path.moveTo(start_point)
 
         if self.port_one.is_input == True:
             point1 = QPointF((self.start_point_x - 100), self.start_point_y)
@@ -44,12 +51,20 @@ class GraphicsLine(QGraphicsItem):
             point2 = QPointF((self.end_point_x + 100), self.end_point_y)
 
         end_point = QPointF(self.end_point_x, self.end_point_y)
-        self.path.cubicTo(point1, point2, end_point)
-        painter.drawPath(self.path)
+        self.test_path.cubicTo(point1, point2, end_point)
+        painter.drawPath(self.shape())
+        self.setPath(self.test_path)
 
+    def shape(self):
+        return self.test_path
+    
+    def midpoint(self, point1, point2):
+        return QPointF((point1.x() + point2.x()) / 2, (point1.y() + point2.y()) / 2)
+    
     def update_pos(self):
         self.start_point_x = self.port_one.port_pos().x()
         self.start_point_y = self.port_one.port_pos().y()
         self.end_point_x = self.port_two.port_pos().x()
         self.end_point_y = self.port_two.port_pos().y()
         self.update()
+        
