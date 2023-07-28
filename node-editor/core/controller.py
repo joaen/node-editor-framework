4e683748349ee2686f3ee2fd2f8070147a4c2ac8
Joaen <joar.engberg@gmail.com>
2023-07-28 16:36:03 +0200

    cleaned up the controller class

--- node-editor/core/controller.py
+++ node-editor/core/controller.py
@@ -1,4 +1,3 @@
-import traceback
 from core.logic_node import LogicNode
 from core.logic_port import LogicPort
 from graphics.graphics_node import GraphicsNode
@@ -15,38 +14,34 @@ class Controller():
 
     @classmethod
     def create_connection(cls, port_1: LogicPort, port_2: LogicPort):
-        try:
-            if port_1.is_connected and port_2.is_connected:
-                print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
-                return None
-            if port_2.is_connected and port_2.is_input:
-                print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
-                return None
-            if port_1.is_connected and port_1.is_input:
-                print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
-                return None
-            elif port_1 == port_2:
-                print("CAN'T CONNECT PORT TO ITSELF")
-                return None
-            elif port_1.is_input and port_2.is_input:
-                print("YOU CAN'T CONNECT TWO INPUT PORTS")
-                return None
-            elif port_1.is_input == False and port_2.is_input == False:
-                print("YOU CAN'T CONNECT TWO OUTPUT PORTS")
-                return None
-            elif port_1.node == port_2.node:
-                print("YOU CAN'T CONNECT NODE TO ITSELF")
-                return None
-            else:
-                port_1.connection = port_2
-                port_2.connection = port_1
-                port_1.is_connected = True
-                port_2.is_connected = True
-                print("Connected: {} || {}".format(port_1.node, port_2.node))
-                return [port_1, port_2]
-        except:
-            traceback.print_exc()
+        if port_1.is_connected and port_2.is_connected:
+            print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
             return None
+        if port_2.is_connected and port_2.is_input:
+            print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
+            return None
+        if port_1.is_connected and port_1.is_input:
+            print("THIS PORT ALREADY HAVE A CONNECTION".format(port_1))
+            return None
+        elif port_1 == port_2:
+            print("CAN'T CONNECT PORT TO ITSELF")
+            return None
+        elif port_1.is_input and port_2.is_input:
+            print("YOU CAN'T CONNECT TWO INPUT PORTS")
+            return None
+        elif port_1.is_input == False and port_2.is_input == False:
+            print("YOU CAN'T CONNECT TWO OUTPUT PORTS")
+            return None
+        elif port_1.node == port_2.node:
+            print("YOU CAN'T CONNECT NODE TO ITSELF")
+            return None
+        else:
+            port_1.connection = port_2
+            port_2.connection = port_1
+            port_1.is_connected = True
+            port_2.is_connected = True
+            print("Connected: {} || {}".format(port_1.node, port_2.node))
+            return [port_1, port_2]
 
     @classmethod
     def break_connection(cls, port_1: LogicPort, port_2: LogicPort):
